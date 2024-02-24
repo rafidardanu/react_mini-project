34a16c7084fd89b3f4e89c963ca018b39101fee4
rafidardanu <rescuephones2016@gmail.com>
2024-02-24 07:07:13 +0700

    delete ai implement

--- src/components/landingpage/Home.jsx
+++ src/components/landingpage/Home.jsx
@@ -1,32 +1,32 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import OpenAI from "openai";
+// import { useState } from "react";
+// import OpenAI from "openai";
 
 function Home() {
-  const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
+  // const openai = new OpenAI({
+  //   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  //   dangerouslyAllowBrowser: true,
+  // });
 
-  const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [prompt, setPrompt] = useState("");
+  // const [result, setResult] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  const handleClick = async () => {
-    setLoading(true);
-    try {
-      const response = await openai.completions.create({
-        model: "text-davinci-003",
-        prompt: `Transaksi aman dan Rekening Bersama di RekberIn: ${prompt}`,
-        temperature: 0.5,
-        max_tokens: 100,
-      });
-      setResult(response.choices[0].text);
-    } catch (error) {
-      console.error(error);
-    }
-    setLoading(false);
-  };
+  // const handleClick = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const response = await openai.completions.create({
+  //       model: "text-davinci-003",
+  //       prompt: `Transaksi aman dan Rekening Bersama di RekberIn: ${prompt}`,
+  //       temperature: 0.5,
+  //       max_tokens: 100,
+  //     });
+  //     setResult(response.choices[0].text);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   setLoading(false);
+  // };
   return (
     <main id="home">
       <div className="container">
@@ -59,19 +59,19 @@ function Home() {
               type="text"
               className="form-control border-1 inputai"
               value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
+              // onChange={(e) => setPrompt(e.target.value)}
               placeholder="Ask anything"
             />
             <button
               className="btn buttonai ms-2"
-              onClick={handleClick}
-              disabled={loading || prompt.length === 0}
+              // onClick={handleClick}
+              // disabled={loading || prompt.length === 0}
             >
               Test
             </button>
           </div>
           <div className="col-8 pt-3">
-            <pre className="result">{result}</pre>
+            {/* <pre className="result">{result}</pre> */}
           </div>
         </div>
       </div>
